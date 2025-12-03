1dc1338efb52305643dbfc9f5c25753393078ee2
Abhijeet Rohilla <nishuinhurry@gmail.com>
2025-12-03 22:23:08 +0530

    revoking API_KEYS

--- src/constant/Constant.jsx
+++ src/constant/Constant.jsx
@@ -2,5 +2,5 @@ export const HAMBURGER_MENU="https://icons.veryicon.com/png/o/miscellaneous/line
 export const YOUTUBE_LOGO="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Logo_of_YouTube_%282015-2017%29.svg/2560px-Logo_of_YouTube_%282015-2017%29.svg.png"
 export const PROFILE="https://media.istockphoto.com/id/1495088043/vector/user-profile-icon-avatar-or-person-icon-profile-picture-portrait-symbol-default-portrait.jpg?s=612x612&w=0&k=20&c=dhV2p1JwmloBTOaGAtaA3AW1KSnjsdMt7-U_3EZElZ0="
 export const SEARCH_ICON="https://static.vecteezy.com/system/resources/thumbnails/009/652/218/small/magnifying-glass-icon-isolated-on-white-background-search-illustration-vector.jpg"
-const API_KEY="AIzaSyAc3cfKYIWg-HvehDpBlRW28s36eHWbIB0"
+import {API_KEY} from "../../Env.js"
 export const YOUTUBE_URL=`https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=IN&maxResults=50&key=${API_KEY}`
